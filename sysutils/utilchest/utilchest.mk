016b2abe4f0b2bfd9e082a42c1fa260135e93a90
Katsuke00 <katsuke00@disroot.org>
2017-10-29 14:19:42 -0200

    fix paths, and use mk.build in utilchest

--- sysutils/utilchest/utilchest.mk
+++ sysutils/utilchest/utilchest.mk
@@ -1,10 +1,71 @@
 <| cat $PORTS/mk/config.mk
 
-build:QV:
-	make AR="$AR" CC="$CC" RANLIB="$RANLIB" PREFIX="$PREFIX" utilchest
+LIBUTILOBJ=\
+	lib/util/chown.o\
+	lib/util/cp.o\
+	lib/util/dir.o\
+	lib/util/ealloc.o\
+	lib/util/mode.o\
+	lib/util/pathcat.o\
+	lib/util/stoll.o
 
-install:QV:
-	make DESTDIR="$ROOT" utilchest-install
+LIBUTFOBJ=\
+	lib/utf/chartorune.o\
+	lib/utf/iscntrlrune.o\
+	lib/utf/isprintrune.o\
+	lib/utf/isvalidrune.o\
+	lib/utf/runetype.o
 
-clean:QV:
-	make clean
+LIB= lib/libutil.a lib/libutf.a
+
+BIN=\
+	src/basename\
+	src/cat\
+	src/chgrp\
+	src/chmod\
+	src/chown\
+	src/chroot\
+	src/clear\
+	src/cp\
+	src/date\
+	src/dirname\
+	src/domainname\
+	src/echo\
+	src/env\
+	src/false\
+	src/head\
+	src/hostname\
+	src/id\
+	src/link\
+	src/ln\
+	src/ls\
+	src/mkdir\
+	src/mkfifo\
+	src/mknod\
+	src/mv\
+	src/nice\
+	src/printenv\
+	src/pwd\
+	src/readlink\
+	src/rm\
+	src/rmdir\
+	src/sleep\
+	src/sync\
+	src/true\
+	src/tty\
+	src/uname\
+	src/unlink\
+	src/whoami\
+	src/yes
+
+OBJ= ${BIN:%=%.o} $LIBUTILOBJ $LIBUTFOBJ
+
+<$PORTS/mk/mk.build
+
+CFLAGS = $CFLAGS -I inc
+LDLIBS = ${LIB}
+
+lib/libutil.a: $LIBUTILOBJ
+lib/libutf.a: $LIBUTFOBJ
+
+build:QV: $LIB all
