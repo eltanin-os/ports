8c2f9ecec853017b806fd1831b8f2a8a6dc2823f
Katsuke00 <katsuke00@disroot.org>
2017-10-31 19:57:32 -0200

    use mk.build in pigz

--- archivers/pigz/pigz.mk
+++ archivers/pigz/pigz.mk
@@ -1,15 +1,30 @@
 <| cat $PORTS/mk/config.mk
 
-all:QV: build
+BINS =\
+	pigz
 
-build:QV:
-	make CC="$CC" CFLAGS="$CPPFLAGS $CFLAGS" LDFLAGS="$LDFLAGS"
+MANS =\
+	1 pigz.1
 
-install:QV:
-	mkdir -p ${ROOT}/${BINDIR} ${ROOT}/${MANDIR}/man1
-	install -c -s -m 555 pigz ${ROOT}/${BINDIR}/pigz
-	install -c -s -m 555 unpigz ${ROOT}/${BINDIR}/unpigz
-	install -c -m 444 pigz.1 ${ROOT}/${MANDIR}/man1
+OBJS =\
+	pigz.o\
+	try.o\
+	yarn.o\
+	zopfli/src/zopfli/blocksplitter.o\
+	zopfli/src/zopfli/cache.o\
+	zopfli/src/zopfli/deflate.o\
+	zopfli/src/zopfli/hash.o\
+	zopfli/src/zopfli/katajainen.o\
+	zopfli/src/zopfli/lz77.o\
+	zopfli/src/zopfli/squeeze.o\
+	zopfli/src/zopfli/tree.o\
+	zopfli/src/zopfli/util.o
 
-clean:QV:
-	make clean
+SYMS =\
+	pigz ${BINDIR}/unpigz
+
+<$PORTS/mk/mk.build
+
+LDLIBS= -lm -lpthread -lz
+
+${BINS}: ${OBJS}
