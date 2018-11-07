62b2f7e11fce275fd969a3ac9d0dd94ead7aef18
Eduardo Francisco <eadwardus@disroot.org>
2018-11-07 08:17:19 -0200

    Add make

---
+++ devel/make/mk/config.h
@@ -0,0 +1,84 @@
+#include <_mk_def.h>
+
+#define FILE_TIMESTAMP_HI_RES 1
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+#define HAVE_DECL_BSD_SIGNAL 1
+#define HAVE_DECL_DLERROR 1
+#define HAVE_DECL_DLOPEN 1
+#define HAVE_DECL_DLSYM 1
+#define HAVE_DECL_SYS_SIGLIST 0
+#define HAVE_DECL__SYS_SIGLIST 0
+#define HAVE_DECL___SYS_SIGLIST 0
+
+#define HAVE_DIRENT_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_GETLOADAVG 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_LOCALE_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MKTEMP 1
+#define HAVE_SA_RESTART 1
+#define HAVE_SETLINEBUF 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_RESOURCE_H 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIMEB_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_UINTMAX_T 1
+#define HAVE_UNISTD_H 1
+#define HAVE_UNSIGNED_LONG_LONG_INT 1
+#define HAVE_VFORK 1
+#define HAVE_WAIT3 1
+#define HAVE_WORKING_FORK 1
+#define HAVE_WORKING_VFORK 1
+#define MAKE_HOST "x86_64-linux-musl"
+#define MAKE_JOBSERVER 1
+#define MAKE_LOAD 1
+#define MAKE_SYMLINKS 1
+#define PACKAGE "make"
+#define PACKAGE_BUGREPORT "bug-make@gnu.org"
+#define PACKAGE_NAME "GNU make"
+#define PACKAGE_STRING "GNU make 4.2"
+#define PACKAGE_TARNAME "make"
+#define PACKAGE_URL "http://www.gnu.org/software/make/"
+#define PACKAGE_VERSION "4.2"
+#define PATH_SEPARATOR_CHAR ':'
+#define RETSIGTYPE void
+#define SCCS_GET "get"
+#define ST_MTIM_NSEC st_mtim.tv_nsec
+#define TIME_WITH_SYS_TIME 1
+
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+#define VERSION "4.2"
+
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
