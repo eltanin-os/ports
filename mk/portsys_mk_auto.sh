33f4f5df764c5213badbf5978d99981d84456834
Eduardo Francisco <eadwardus@disroot.org>
2018-11-19 17:54:22 -0200

    Add more flags inside portsys_mk_*.sh

--- mk/portsys_mk_auto.sh
+++ mk/portsys_mk_auto.sh
@@ -6,6 +6,10 @@ _L="$LDFLAGS"
 
 . ${PORTS}/mk/config.mk
 
+[ -z "$_C" ] && _C="$CFLAGS"
+[ -z "$_P" ] && _P="$CPPFLAGS"
+[ -z "$_L" ] && _L="$LDFLAGS"
+
 CONFIGURE="./configure"
 
 if [ -z "$_PORTSYS_MK_AUTO_NOBUILDDIR" ]; then
@@ -29,8 +33,9 @@ if [ "$1" == "install" ]; then
 	make DESTDIR="$DESTDIR" $@
 else
 	[ ! -n "$PREFIX" ] && PREFIX="/"
-	env CC="$CC" CXX="$CXX" CFLAGS="$_C"          \
+	env AR="$AR" CC="$CC" CXX="$CXX" CFLAGS="$_C" \
 	    CPPFLAGS="$_P" LDFLAGS="$_L" YACC="$YACC" \
+	    RANLIB="$RANLIB" STRIP="$STRIP"           \
 	    $CONFIGURE --prefix="$PREFIX"             \
 	               --bindir="$BINDIR"             \
 	               --sbindir="$BINDIR"            \
--- mk/portsys_mk_make.sh
+++ mk/portsys_mk_make.sh
@@ -6,19 +6,26 @@ _L="$LDFLAGS"
 
 . ${PORTS}/mk/config.mk
 
-make -j $nprocs\
-     CC="$CC"\
-     CXX="$CXX"\
-     CFLAGS="$_C"\
-     CPPFLAGS="$_P"\
-     LDFLAGS="$_L"\
-     YACC="$YACC"\
-     PREFIX="$PREFIX"\
-     BINDIR="$BINDIR"\
-     LIBDIR="$LIBDIR"\
-     ETCDIR="$ETCDIR"\
-     DFLDIR="$DFLDIR"\
-     MANDIR="$MANDIR"\
-     INCDIR="$INCDIR"\
-     DESTDIR="$DESTDIR"\
+[ -z "$_C" ] && _C="$CFLAGS"
+[ -z "$_P" ] && _P="$CPPFLAGS"
+[ -z "$_L" ] && _L="$LDFLAGS"
+
+make -j $nprocs         \
+     AR="$AR"           \
+     CC="$CC"           \
+     CXX="$CXX"         \
+     CFLAGS="$_C"       \
+     CPPFLAGS="$_P"     \
+     LDFLAGS="$_L"      \
+     YACC="$YACC"       \
+     STRIP="$STRIP"     \
+     RANLIB="$RANLIB"   \
+     PREFIX="$PREFIX"   \
+     BINDIR="$BINDIR"   \
+     LIBDIR="$LIBDIR"   \
+     ETCDIR="$ETCDIR"   \
+     DFLDIR="$DFLDIR"   \
+     MANDIR="$MANDIR"   \
+     INCDIR="$INCDIR"   \
+     DESTDIR="$DESTDIR" \
      $@
