a8d7bcfa2bbedff60ac349f03f0cc590c355f009
Eduardo Francisco <eadwardus@disroot.org>
2020-07-17 16:57:18 -0300

    Fix bugs

--- mk/bin/code/helper.c
+++ mk/bin/code/helper.c
@@ -13,14 +13,14 @@ enum {
 #define fmtstr(...) fmtstr_(__VA_ARGS__, nil)
 
 struct cfg {
-	ctype_arr arr;
 	ctype_ioq ioq;
+	ctype_arr *arr;
+	ctype_fd fd;
 	int tag;
-	char buf[C_BIOSIZ];
+	char buf[C_SMALLBIOSIZ];
 };
 
 static ctype_cdb cdb;
-
 static char *dbdir;
 static char *ports;
 
@@ -39,6 +39,172 @@ fmtstr_(char *fmt, ...)
 	return c_arr_data(&arr);
 }
 
+/* conf routines */
+static void
+findstart(struct cfg *c)
+{
+	c_sys_seek(c->fd, 0, C_SEEKSET);
+	c_ioq_init(&c->ioq, c->fd, c->buf, sizeof(c->buf), &c_sys_read);
+}
+
+static char *
+getline(struct cfg *p, ctype_status *r)
+{
+	char *s;
+
+	c_arr_trunc(p->arr, 0, sizeof(uchar));
+	if ((*r = c_ioq_getln(&p->ioq, p->arr)) < 0)
+		return (void *)-1;
+	if (!*r) {
+		*r = -1;
+		return nil;
+	}
+	s = c_arr_data(p->arr);
+	s[c_arr_bytes(p->arr) - 1] = 0;
+	return s;
+}
+
+void
+cfginit(struct cfg *c, ctype_fd fd)
+{
+	/* c_arr_init(&c->arr, nil, 0); */
+	c_ioq_init(&c->ioq, fd, c->buf, sizeof(c->buf), &c_sys_read);
+	c->tag = 0;
+	c->fd = fd;
+}
+
+char *
+cfgfind(struct cfg *c, char *k)
+{
+	ctype_status r;
+	char *p, *s;
+
+	findstart(c);
+	for (;;) {
+		s = getline(c, &r);
+		if (r < 0)
+			return s;
+		if (!(p = c_mem_chr(s, c_arr_bytes(c->arr), ':')))
+			continue;
+		*p++ = 0;
+		if (!(c_str_cmp(s, C_USIZEMAX, k)))
+			return p;
+	}
+}
+
+char *
+cfgfindtag(struct cfg *c, char *k)
+{
+	ctype_status r;
+	char *s;
+
+	if (!c->tag) {
+		findstart(c);
+		do {
+			s = getline(c, &r);
+			if (r < 0)
+				return s;
+			if (!c_str_cmp(s, C_USIZEMAX, k))
+				++c->tag;
+		} while(!c->tag);
+	}
+	s = getline(c, &r);
+	if (r < 0)
+		return s;
+	if (*s == '}') {
+		c->tag = 0;
+		return nil;
+	}
+	if (*s != '\t') {
+		c->tag = 0;
+		errno = C_EINVAL;
+		return (void *)-1;
+	}
+	return ++s;
+}
+
+/* easy conf routines */
+static void *
+getcfg(char *s)
+{
+	static ctype_arr arr;
+	struct cfg *c;
+	ctype_fd fd;
+
+	if (!(c = c_std_alloc(1, sizeof(c))))
+		c_err_die(1, "c_std_alloc");
+	c->arr = &arr;
+
+	s = fmtstr("%s/vars", s);
+	if ((fd = c_sys_open(s, C_OREAD, 0)) < 0)
+		c_err_die(1, "c_sys_open %s", s);
+	cfginit(c, fd);
+	return c;
+}
+
+static void
+closecfg(struct cfg *c)
+{
+	c_sys_close(c->fd);
+	c_std_free(c);
+}
+
+static char *
+getkey(struct cfg *c, char *k, int istag)
+{
+	usize len;
+	int ch;
+	char *e, *p, *s;
+
+	if ((s = (istag ? cfgfindtag : cfgfind)(c, k)) == (void *)-1)
+		c_err_die(1, "getkey");
+	if (!s)
+		return nil;
+	while ((p = c_str_chr(s, c_arr_bytes(c->arr), '$'))) {
+		len = p - s;
+		if (!(s = c_str_dup(s, c_arr_bytes(c->arr))))
+			c_err_die(1, "c_str_dup");
+		p = s + len;
+		*p++ = 0;
+		for (e = p; *e && c_str_casechr(ALPHA, 26, *e); ++e) ;
+		if (*e) {
+			ch = *e;
+			*e++ = 0;
+			s = fmtstr("%s%s%c%s", s, getkey(c, p, 0), ch, e);
+		} else {
+			s = fmtstr("%s%s", s, getkey(c, p, 0));
+		}
+		len = c_arr_bytes(c->arr);
+		c_arr_trunc(c->arr, 0, sizeof(uchar));
+		if (c_dyn_fmt(c->arr, "%s", s) < 0)
+			c_err_die(1, "c_dyn_fmt");
+		c_std_free(s);
+		s = c_arr_data(c->arr);
+	}
+	return s;
+}
+
+static char *
+getpath(char *s)
+{
+	static ctype_arr arr;
+	usize len, pos;
+
+	if (c_cdb_find(&cdb, s, c_str_len(s, -1)) <= 0)
+		return nil;
+	len = c_cdb_datalen(&cdb);
+	c_arr_trunc(&arr, 0, sizeof(uchar));
+	if (c_dyn_ready(&arr, len + 1, sizeof(uchar)) < 0)
+		c_err_die(1, "c_dyn_ready");
+	s = c_arr_data(&arr);
+	pos = c_cdb_datapos(&cdb);
+	if (c_cdb_read(&cdb, s, len, pos) < 0)
+		c_err_die(1, "c_cdb_read %d %d", len, pos);
+	s[len] = 0;
+	return s;
+}
+
+/* arg routines */
 static void
 gencache(char *path)
 {
@@ -93,189 +259,6 @@ gencache(char *path)
 		c_err_die(1, "c_sys_rename %s %s", tmp, path);
 }
 
-/* conf routines */
-static void
-findstart(struct cfg *p)
-{
-	ctype_fd fd;
-
-	fd = c_ioq_fileno(&p->ioq);
-	c_sys_seek(fd, 0, C_SEEKSET);
-	c_ioq_init(&p->ioq, fd, p->buf, sizeof(p->buf), &c_sys_read);
-}
-
-static ctype_fssize
-getpos(struct cfg *p)
-{
-	return c_sys_seek(c_ioq_fileno(&p->ioq), 0, C_SEEKCUR);
-}
-
-static void
-tagsetpos(struct cfg *p, ctype_fssize off)
-{
-	findstart(p);
-	c_sys_seek(c_ioq_fileno(&p->ioq), off, C_SEEKSET);
-	++p->tag;
-}
-
-static char *
-getline(struct cfg *p, ctype_status *r)
-{
-	char *s;
-
-	c_arr_trunc(&p->arr, 0, sizeof(uchar));
-	if ((*r = c_ioq_getln(&p->ioq, &p->arr)) < 0)
-		return (void *)-1;
-	if (!*r)
-		goto end;
-
-	s = c_arr_data(&p->arr);
-	s[c_arr_bytes(&p->arr) - 1] = 0;
-	return s;
-end:
-	*r = -1;
-	return nil;
-}
-
-static void
-cfginit(struct cfg *p, ctype_fd fd)
-{
-	/* c_arr_init(&p->arr, nil, 0); */
-	c_ioq_init(&p->ioq, fd, p->buf, sizeof(p->buf), &c_sys_read);
-	p->tag = 0;
-}
-
-static char *
-cfgfind(struct cfg *cfg, char *k)
-{
-	ctype_status r;
-	char *p, *s;
-
-	findstart(cfg);
-	for (;;) {
-		s = getline(cfg, &r);
-		if (r < 0)
-			return s;
-		if (!(p = c_mem_chr(s, c_arr_bytes(&cfg->arr), ':')))
-			continue;
-		*p++ = 0;
-		if (!(c_str_cmp(s, C_USIZEMAX, k)))
-			return p;
-	}
-}
-
-static char *
-cfgfindtag(struct cfg *cfg, char *k)
-{
-	ctype_status r;
-	char *s;
-
-	if (!cfg->tag) {
-		findstart(cfg);
-		do {
-			s = getline(cfg, &r);
-			if (r < 0)
-				return s;
-			if (!c_str_cmp(s, C_USIZEMAX, k))
-				++cfg->tag;
-		} while(!cfg->tag);
-	}
-	s = getline(cfg, &r);
-	if (r < 0)
-		return s;
-	if (*s == '}') {
-		cfg->tag = 0;
-		return nil;
-	}
-	if (*s != '\t') {
-		cfg->tag = 0;
-		errno = C_EINVAL;
-		return (void *)-1;
-	}
-	return ++s;
-}
-
-/* args utils routines */
-static char *
-getpath(char *s)
-{
-	static ctype_arr arr;
-	usize len, pos;
-
-	if (c_cdb_find(&cdb, s, c_str_len(s, -1)) <= 0)
-		return nil;
-	len = c_cdb_datalen(&cdb);
-	c_arr_trunc(&arr, 0, sizeof(uchar));
-	if (c_dyn_ready(&arr, len + 1, sizeof(uchar)) < 0)
-		c_err_die(1, "c_dyn_ready");
-	s = c_arr_data(&arr);
-	pos = c_cdb_datapos(&cdb);
-	if (c_cdb_read(&cdb, s, len, pos) < 0)
-		c_err_die(1, "c_cdb_read %d %d", len, pos);
-	s[len] = 0;
-	return s;
-
-}
-
-static struct cfg *
-getcfg(char *s)
-{
-	static struct cfg cfg;
-	ctype_fd fd;
-
-	if (c_arr_data(&cfg.arr))
-		c_sys_close(c_ioq_fileno(&cfg.ioq));
-
-	s = fmtstr("%s/vars", s);
-	if ((fd = c_sys_open(s, C_OREAD, 0)) < 0)
-		c_err_die(1, "c_sys_open %s", s);
-
-	cfginit(&cfg, fd);
-	return &cfg;
-}
-
-static char *
-getkey(struct cfg *cfg, char *k, int istag)
-{
-	ctype_fssize off;
-	usize len;
-	int ch;
-	char *(*func)(struct cfg *, char *);
-	char *e, *p, *s;
-
-	func = istag ? cfgfindtag : cfgfind;
-	if ((s = func(cfg, k)) == (void *)-1)
-		c_err_die(1, "getkey");
-	if (!s)
-		return nil;
-
-	while ((p = c_str_chr(s, -1, '$'))) {
-		len = p - s;
-		if (!(s = c_str_dup(s, c_arr_bytes(&cfg->arr))))
-			c_err_die(1, "c_str_dup");
-		p = s + len;
-		*p++ = 0;
-		for (e = p; *e && c_str_casechr(ALPHA, 26, *e); ++e) ;
-		off = getpos(cfg);
-		if (*e) {
-			ch = *e;
-			*e++ = 0;
-			s = fmtstr("%s%s%c%s", s, getkey(cfg, p, 0), ch, e);
-		} else {
-			s = fmtstr("%s%s", s, getkey(cfg, p, 0));
-		}
-		len = c_arr_bytes(&cfg->arr);
-		c_arr_trunc(&cfg->arr, 0, sizeof(uchar));
-		if (c_dyn_fmt(&cfg->arr, "%s", s) < 0)
-			c_err_die(1, "c_dyn_fmt");
-		c_std_free(s);
-		s = c_arr_data(&cfg->arr);
-		tagsetpos(cfg, off + len);
-	}
-	return s;
-}
-
-/* args routines */
 static int
 checknode(ctype_node *p, char *s)
 {
@@ -292,44 +275,47 @@ checknode(ctype_node *p, char *s)
 static int
 populatedeps(ctype_node **np, ctype_node *list)
 {
-	struct cfg *cfg;
+	struct cfg *c;
 	ctype_stat st;
-	ctype_node *wp;
-	ctype_fssize off;
+	ctype_node *deps, *wp;
+	usize len, n;
 	ctype_status r;
-	char *path, *s;
+	char *p, *s;
 
 	if (!list)
 		return 0;
 
+	n = r = 0;
 	wp = list->next;
-	r = 0;
 	do {
-		cfg = getcfg(wp->p);
-		if (!(s = getkey(cfg, "mdeps{", 1)))
+		c = getcfg(wp->p);
+		if (!(s = getkey(c, "mdeps{", 1)))
 			break;
 		do {
-			path = fmtstr("%s/%s-dev", dbdir, s);
-			if (!c_sys_stat(&st, path))
+			p = fmtstr("%s/%s-dev", dbdir, s);
+			if (!c_sys_stat(&st, p))
 				continue;
-			if (!(path = getpath(s))) {
+			if (!(p = getpath(s))) {
 				r = c_err_warnx("%s: package not found", s);
 				continue;
 			}
-			if (checknode(*np, path))
+			if (checknode(*np, p))
 				continue;
-			off = c_str_len(path, -1) + 1;
-			if (c_adt_ltpush(np, c_adt_lnew(path, off)) < 0)
-				c_err_die(1, "c_adt_lpush %s", path);
-			off = getpos(cfg);
-			populatedeps(np, *np);
-			cfg = getcfg(wp->p);
-			tagsetpos(cfg, off + c_str_len(s, -1) + 1);
-		} while ((s = getkey(cfg, nil, 1)));
-		c_sys_close(c_ioq_fileno(&cfg->ioq));
+			len = c_str_len(p, -1) + 1;
+			if (c_adt_ltpush(np, c_adt_lnew(p, len)) < 0)
+				c_err_die(1, "c_adt_ltpush %s", p);
+			++n;
+		} while ((s = getkey(c, nil, 1)));
+		closecfg(c);
 	} while ((wp = wp->next)->prev);
 
-	return r ? (c_sys_exit(1), -1) : 0;
+	if (n) {
+		deps = nil;
+		r |= populatedeps(&deps, *np);
+		c_adt_ltpush(np, deps);
+	}
+
+	return r ? (c_std_exit(1), -1) : 0;
 }
 
 static void
@@ -339,8 +325,8 @@ printdeps(char **argv)
 	ctype_status r;
 	char *s;
 
-	list = nil;
 	r = 0;
+	list = nil;
 	for (; *argv; ++argv) {
 		if (!(s = getpath(*argv))) {
 			r = c_err_warnx("%s: package not found", *argv);
@@ -351,7 +337,7 @@ printdeps(char **argv)
 	}
 
 	if (r)
-		c_sys_exit(1);
+		c_std_exit(1);
 
 	deps = nil;
 	populatedeps(&deps, list);
@@ -371,19 +357,20 @@ usage(void)
 	c_ioq_fmt(ioq2,
 	    "usage: %s [-u] pkg\n"
 	    "       %s [-u] -k|-t key pkg\n"
-	    "       %s [-u] -d pkg ...\n",
-	    "          -u\n",
-	    c_std_getprogname(), c_std_getprogname(), c_std_getprogname());
+	    "       %s [-u] -d pkg ...\n"
+	    "       %s -u\n",
+	    c_std_getprogname(), c_std_getprogname(),
+	    c_std_getprogname(), c_std_getprogname());
 	c_std_exit(1);
 }
 
 ctype_status
 main(int argc, char **argv)
 {
-	struct cfg *cfg;
+	struct cfg *c;
 	ctype_fd fd;
 	int mode, uflag;
-	char *key, *s;
+	char *k, *s;
 
 	c_std_setprogname(argv[0]);
 
@@ -395,11 +382,11 @@ main(int argc, char **argv)
 		mode = DMODE;
 		break;
 	case 'k':
-		key = C_EARGF(usage());
+		k = C_EARGF(usage());
 		mode = KMODE;
 		break;
 	case 't':
-		key = C_EARGF(usage());
+		k = C_EARGF(usage());
 		mode = TMODE;
 		break;
 	case 'u':
@@ -408,7 +395,6 @@ main(int argc, char **argv)
 	default:
 		usage();
 	} C_ARGEND
-
 
 	if (!(ports = c_std_getenv("PORTS")))
 		c_err_diex(1, "missing PORTS environmental variable");
@@ -422,12 +408,15 @@ main(int argc, char **argv)
 			return 0;
 	}
 
-	if (!(dbdir = c_std_getenv("DBDIR")))
-		c_err_diex(1, "missing DBDIR environmental variable");
+	if (!argc)
+		usage();
 
 	if ((fd = c_sys_open(s, C_OREAD, 0)) < 0)
 		c_err_die(1, "c_sys_open %s", s);
 	c_cdb_init(&cdb, fd);
+
+	if (!(dbdir = c_std_getenv("DBDIR")))
+		c_err_diex(1, "missing DBDIR environmental variable");
 
 	switch (mode) {
 	case PMODE:
@@ -441,19 +430,13 @@ main(int argc, char **argv)
 	case KMODE:
 		if (!(s = getpath(*argv)))
 			c_err_diex(1, "%s: package not found", *argv);
-		cfg = getcfg(s);
-		c_ioq_fmt(ioq1, "%s\n", getkey(cfg, key, 0));
-		c_sys_close(c_ioq_fileno(&cfg->ioq));
-		break;
-	case TMODE:
-		if (!(s = getpath(*argv)))
-			c_err_diex(1, "%s: package not found", *argv);
-		cfg = getcfg(s);
-		if (!(s = getkey(cfg, fmtstr("%s{", key), 1)))
+		c = getcfg(s);
+		if (!(s = getkey(c, fmtstr("%s{", k), 1)))
 			break;
 		do {
 			c_ioq_fmt(ioq1, "%s\n", s);
-		} while ((s = getkey(cfg, nil, 1)));
+		} while ((s = getkey(c, nil, 1)));
+		closecfg(c);
 	}
 	c_ioq_flush(ioq1);
 	return 0;
